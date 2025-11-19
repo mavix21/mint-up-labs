b0bd73b748bbf2a2013b45c1a12735b0fd1f0333
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-19 01:06:45 -0300

    refactor: remove better auth clients

--- apps/mini-lemon/src/lib/auth-client.ts
+++
@@ -1,7 +0,0 @@
-import { convexClient } from "@convex-dev/better-auth/client/plugins";
-import { siweClient } from "better-auth/client/plugins";
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-  plugins: [convexClient(), siweClient()],
-});
