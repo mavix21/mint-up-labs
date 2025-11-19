b994fc54945305b93da64bb796e3616a2fce38be
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-19 14:46:06 -0300

    feat: update Home component to render EventsContainer

--- apps/mini-lemon/app/(app)/home/page.tsx
+++ apps/mini-lemon/app/(app)/home/page.tsx
@@ -1,3 +1,11 @@
+import { EventsContainer } from "@/app/_pages/my-events/ui/events-container";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="bg-background flex min-h-screen flex-col pb-24">
+      <main className="flex-1">
+        <EventsContainer />
+      </main>
+    </div>
+  );
 }
