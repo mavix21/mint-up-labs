ff64f161627a69cd68c33da495c6e0866163dca7
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-19 01:11:15 -0300

    fix: convex client provider path

--- apps/mini-lemon/app/layout.tsx
+++ apps/mini-lemon/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
 
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ConvexClientProvider } from "./_shared/ui/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider session={null}>{children}</ConvexClientProvider>
       </body>
     </html>
   );
