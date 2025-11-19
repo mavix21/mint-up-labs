1aff1054eb19cd889079a04fde1516c290b45d57
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-19 16:51:45 -0300

    fix: change default theme to dark in ThemeProvider

--- apps/mini-lemon/app/layout.tsx
+++ apps/mini-lemon/app/layout.tsx
@@ -45,7 +45,7 @@ export default async function RootLayout({
             <MiniAppProvider>
               <ThemeProvider
                 attribute="class"
-                defaultTheme="system"
+                defaultTheme="dark"
                 enableSystem
                 disableTransitionOnChange
               >
