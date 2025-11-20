40e12d9927e163532151d0720c24e74f3fbf1767
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-20 17:19:59 -0300

    feat: add constants for BASE_CHAIN_ID and contract addresses

---
+++ apps/mini-lemon/app/_shared/lib/constants.ts
@@ -0,0 +1,9 @@
+import type { Address } from "viem";
+
+export const BASE_CHAIN_ID = 84532;
+
+export const USDC_CONTRACT_ADDRESS: Address =
+  "0x833589fCD6EDb6B5eD0E328aBf3cbe6FADf4c6f3" as const;
+
+export const MINTUP_FACTORY_CONTRACT_ADDRESS =
+  "0x5Bf66d335822BEAD0848AaA92A15fe842b554871" as const;
